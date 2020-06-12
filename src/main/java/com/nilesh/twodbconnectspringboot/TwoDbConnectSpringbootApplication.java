c5b67afeaa51f8aa1d4cc42768edf90a79785e4f
NileshJorwar <jorwarnilesh@gmail.com>
2020-06-12 10:18:49 -0500

    Initial commit

---
+++ src/main/java/com/nilesh/twodbconnectspringboot/TwoDbConnectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nilesh.twodbconnectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDbConnectSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoDbConnectSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nilesh/twodbconnectspringboot/TwoDbConnectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilesh.twodbconnectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoDbConnectSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
